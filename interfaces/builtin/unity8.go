f98b681a1ed588fb9a97d6403e7b8e276f1fb67c
Ted Gould <ted@gould.cx>
2016-09-22 16:31:12 -0500

    Getting all of the interface info from other sources

--- interfaces/builtin/unity8.go
+++ interfaces/builtin/unity8.go
@@ -25,6 +25,218 @@ import (
 	"github.com/snapcore/snapd/interfaces"
 )
 
+var unity8ConnectedPlugAppArmor = []byte(`
+  #include <abstractions/base>
+  #include <abstractions/fonts>
+  #include <abstractions/X>
+
+  # Apps fail to start when linked against newer curl/gnutls if we don't allow
+  # this. (LP: #1350152)
+  #include <abstractions/openssl>
+
+  # Mir-specific stuff
+  #include <abstractions/mir>
+
+  # Needed by native GL applications on Mir
+  owner /{,var/}run/user/*/mir_socket rw,
+
+  # Hardware-specific accesses
+  #include "/usr/share/apparmor/hardware/graphics.d"
+
+  #
+  # IPC rules common for all apps
+  #
+  # Allow connecting to session bus and where to connect to services
+  #include <abstractions/dbus-session-strict>
+
+  # Allow connecting to system bus and where to connect to services. Put these
+  # here so we don't need to repeat these rules in multiple places (actual
+  # communications with any system services is mediated elsewhere). This does
+  # allow apps to brute-force enumerate system services, but our system
+  # services aren't a secret.
+  #include <abstractions/dbus-strict>
+
+  # Unity shell
+  dbus (send)
+       bus=session
+       path="/BottomBarVisibilityCommunicator"
+       interface="org.freedesktop.DBus.{Introspectable,Properties}"
+       peer=(name=com.canonical.Shell.BottomBarVisibilityCommunicator,label=unconfined),
+  dbus (receive)
+       bus=session
+       path="/BottomBarVisibilityCommunicator"
+       interface="com.canonical.Shell.BottomBarVisibilityCommunicator"
+       peer=(label=unconfined),
+  # on screen keyboard (OSK)
+  dbus (send)
+       bus=session
+       path="/org/maliit/server/address"
+       interface="org.freedesktop.DBus.Properties"
+       member=Get
+       peer=(name=org.maliit.server,label=unconfined),
+  unix (connect, receive, send)
+       type=stream
+       peer=(addr="@/tmp/maliit-server/dbus-*"),
+
+  # clipboard (LP: #1371170)
+  dbus (receive, send)
+       bus=session
+       path="/com/canonical/QtMir/Clipboard"
+       interface="com.canonical.QtMir.Clipboard"
+       peer=(label=unconfined),
+  dbus (receive, send)
+       bus=session
+       path="/com/canonical/QtMir/Clipboard"
+       interface="org.freedesktop.DBus.{Introspectable,Properties}"
+       peer=(label=unconfined),
+
+  # usensors
+  dbus (send)
+       bus=session
+       path=/com/canonical/usensord/haptic
+       interface=com.canonical.usensord.haptic
+       peer=(label=unconfined),
+
+  # URL dispatcher. All apps can call this since:
+  # a) the dispatched application is launched out of process and not
+  #    controllable except via the specified URL
+  # b) the list of url types is strictly controlled
+  # c) the dispatched application will launch in the foreground over the
+  #    confined app
+  dbus (send)
+       bus=session
+       path="/com/canonical/URLDispatcher"
+       interface="com.canonical.URLDispatcher"
+       member="DispatchURL"
+       peer=(label=unconfined),
+
+  # This is needed when the app is already running and needs to be passed in
+  # a URL to open. This is most often used with content-hub providers and
+  # url-dispatcher, but is actually supported by Qt generally (though because
+  # we don't allow the send a malicious app can't send this to another app).
+  dbus (receive)
+       bus=session
+       path=/@{APP_ID_DBUS}
+       interface="org.freedesktop.Application"
+       member="Open"
+       peer=(label=unconfined),
+
+  # This is needed for apps to interact with the Launcher (eg, for the counter)
+  dbus (receive, send)
+       bus=session
+       path=/com/canonical/unity/launcher/@{APP_ID_DBUS}
+       peer=(label=unconfined),
+
+  # Untrusted Helpers are 3rd party apps that run in a different confinement
+  # context and are in a separate Mir session from the calling app (eg, an
+  # app that uses a content provider from another app). These helpers use
+  # Trusted Prompt Sessions to overlay their window over the calling app and
+  # need to get the Mir socket that was setup by the associated trusted helper
+  # (eg, content-hub). Typical consumers are content-hub providers,
+  # pay-service, url-dispatcher and possibly online-accounts.
+  # LP: #1462492 - this rule is suboptimal and should not be needed once we
+  # move to socket activation or FD passing
+  dbus (receive, send)
+       path=/com/canonical/UbuntuAppLaunch/@{APP_ID_DBUS}/*
+       interface="com.canonical.UbuntuAppLaunch.SocketDemangler"
+       member="GetMirSocket"
+       bus=session
+       peer=(label=unconfined),
+  # Allow access to the socket-demangler (needed for the above)
+  /usr/lib/@{multiarch}/ubuntu-app-launch/socket-demangler rmix,
+
+  # TODO: finetune this
+  dbus (send)
+       bus=session
+       peer=(name=org.a11y.Bus,label=unconfined),
+  dbus (receive)
+       bus=session
+       interface=org.a11y.atspi**
+       peer=(label=unconfined),
+  dbus (receive, send)
+       bus=accessibility
+       peer=(label=unconfined),
+
+  # Deny potentially dangerous access
+  deny dbus bus=session
+            path=/com/canonical/[Uu]nity/[Dd]ebug**,
+  audit deny dbus bus=session
+                  interface="com.canonical.snapdecisions",
+  deny dbus (send)
+       bus=session
+       interface="org.gnome.GConf.Server",
+
+  # LP: #1433590
+  deny dbus bus=system
+            path="/org/freedesktop/Accounts",
+
+  # LP: #1378823
+  deny dbus (bind)
+       name="org.freedesktop.Application",
+
+  # Allow access to the PasteBoard
+  dbus (receive, send)
+       bus=session
+       interface="com.ubuntu.content.dbus.Service"
+       path="/"
+       member="CreatePaste"
+       peer=(label=unconfined),
+  dbus (receive, send)
+       bus=session
+       interface="com.ubuntu.content.dbus.Service"
+       path="/"
+       member="GetPasteData"
+       peer=(label=unconfined),
+  dbus (receive, send)
+       bus=session
+       interface="com.ubuntu.content.dbus.Service"
+       path="/"
+       member="GetLatestPasteData"
+       peer=(label=unconfined),
+  dbus (receive, send)
+       bus=session
+       interface="com.ubuntu.content.dbus.Service"
+       path="/"
+       member="PasteFormats"
+       peer=(label=unconfined),
+  dbus (receive)
+       bus=session
+       interface="com.ubuntu.content.dbus.Service"
+       path="/"
+       member="PasteFormatsChanged"
+       peer=(label=unconfined),
+
+  #
+  # end DBus rules common for all apps
+  #
+`)
+
+var secCompDBus = []byte(`
+# dbus
+connect
+getsockname
+recvmsg
+send
+sendto
+sendmsg
+socket
+`)
+
+/* TODO: Switch to putting the actual name of the snap there on connect */
+var unity8ConnectedSlotAppArmor = []byte(`
+dbus (receive, send)
+    bus=system
+    peer=(label=snap.*),
+`)
+
+var unity8SlotAppArmor = []byte(`
+  # Mir-specific stuff
+  #include <abstractions/mir>
+
+  # Needed by native GL applications on Mir
+  owner /{,var/}run/user/*/mir_socket rw,
+`)
+
 type Unity8Interface struct{}
 
 func (iface *Unity8Interface) Name() string {
@@ -32,33 +244,33 @@ func (iface *Unity8Interface) Name() string {
 }
 
 func (iface *Unity8Interface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
-	switch securitySystem {
-	case interfaces.SecurityDBus, interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityUDev, interfaces.SecurityMount:
-		return nil, nil
-	}
 	return nil, nil
 }
 
 func (iface *Unity8Interface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	switch securitySystem {
-	case interfaces.SecurityDBus, interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityUDev, interfaces.SecurityMount:
-		return nil, nil
+	case interfaces.SecurityAppArmor:
+		return unity8ConnectedPlugAppArmor, nil
+	case interfaces.SecuritySecComp:
+		return secCompDBus, nil
 	}
 	return nil, nil
 }
 
 func (iface *Unity8Interface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	switch securitySystem {
-	case interfaces.SecurityDBus, interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityUDev, interfaces.SecurityMount:
-		return nil, nil
+	case interfaces.SecurityAppArmor:
+		return unity8SlotAppArmor, nil
+	case interfaces.SecuritySecComp:
+		return secCompDBus, nil
 	}
 	return nil, nil
 }
 
 func (iface *Unity8Interface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
 	switch securitySystem {
-	case interfaces.SecurityDBus, interfaces.SecurityAppArmor, interfaces.SecuritySecComp, interfaces.SecurityUDev, interfaces.SecurityMount:
-		return nil, nil
+	case interfaces.SecurityAppArmor:
+		return unity8SlotAppArmor, nil
 	}
 	return nil, nil
 }
@@ -67,6 +279,9 @@ func (iface *Unity8Interface) SanitizePlug(plug *interfaces.Plug) error {
 	if iface.Name() != plug.Interface {
 		panic(fmt.Sprintf("slot is not of interface %q", iface))
 	}
+
+	/* TODO: Check to ensure there is a desktop file */
+	/* TODO: Check to ensure it is on a command not a package */
 
 	return nil
 }
